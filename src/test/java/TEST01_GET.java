c21cb93936da5bf237266cad1432ec5e9c7c91ed
samparker123 <shivammishra1131@yahoo.in>
2021-10-13 17:21:23 +0530

    add one more condition

--- src/test/java/TEST01_GET.java
+++ src/test/java/TEST01_GET.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class TEST01_GET {
 
@@ -28,7 +30,7 @@ public class TEST01_GET {
 	@Test
 	void Test_02() {
 		
-		given().get("https://reqres.in/api/users?page=2").then().statusCode(200);
+		given().get("https://reqres.in/api/users?page=2").then().statusCode(200).body("data.id[0]", equalTo(7));
 	}
 	
 	
